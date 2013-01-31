1d5c1cb8f0ef9ed7a3c6c6f3e2c5875d9875e6d8
Micah Lee <micahflee@riseup.net>
2013-01-31 13:22:24 -0800

    added pianobar config

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# git stuff
 cp bash_aliases ~/.bash_aliases
 cp gitconfig ~/.gitconfig
+
+# screen
 cp screenrc ~/.screenrc
+
+# vim stuff
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim
 cp -r .vim/* ~/.vim/
+
+# pianobar
+mkdir -p ~/.config/pianobar
+cp config/pianobar/config ~/.config/pianobar/
+
