1cbf4bad1e17247340510cf8b95cce62234664f8
Micah Lee <micah@micahflee.com>
2017-05-23 15:19:43 -0700

    Fix git prompt

--- install.sh
+++ install.sh
@@ -11,11 +11,5 @@ cp .vimrc ~/
 
 # set up a basic git bash prompt
 if ! grep --quiet parse_git_branch ~/.bashrc; then
-    echo "
-# git bash prompt
-parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-export PS1=\"\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ \"
-" >> ~/.bashrc
+    cat .gitprompt >> ~/.bashrc
 fi
